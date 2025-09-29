80f3c4799612a03bb2113a29696b8b85cbe1c532
RykerWilder <fabiomasciarelliwd@gmail.com>
2025-09-29 20:54:33 +0200

    fix: messages

--- modules/utilities.py
+++ modules/utilities.py
@@ -9,10 +9,10 @@ def execute_speedtest():
     
     # download
     download_speed = st.download() / 1_000_000  
-    print(f"La velocità in download è {download_speed:.2f} Mbps")
-    voice.text_to_speech(f"La velocità in download è {download_speed:.2f} Mbps")
+    print(f"Download speed is {download_speed:.2f} Mbps")
+    voice.text_to_speech(f"Download speed is {download_speed:.2f} Mbps")
     
     # upload
     upload_speed = st.upload() / 1_000_000 
-    print(f"La velocità in upload è {upload_speed:.2f} Mbps")
-    voice.text_to_speech(f"La velocità in upload è {upload_speed:.2f} Mbps")
+    print(f"Upload speed is {upload_speed:.2f} Mbps")
+    voice.text_to_speech(f"Upload speed is {upload_speed:.2f} Mbps")
