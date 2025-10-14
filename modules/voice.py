66fc3a45493d63053f29458e7a0733922afa6dca
RykerWilder <fabiomasciarelliwd@gmail.com>
2025-10-14 17:42:33 +0200

    update microphone configuration

--- modules/voice.py
+++ modules/voice.py
@@ -3,25 +3,21 @@ import pyttsx3
 
 class Voice:
     def speech_recognizer(self):
-
         # init
         r = sr.Recognizer()
-
         # microphone config
         with sr.Microphone() as source:
             print("Listening...")
-
             # ambient noise
             r.adjust_for_ambient_noise(source, duration=2)
-
-            r.energy_threshold = 400  
+            r.energy_threshold = 200   
             r.dynamic_energy_threshold = True
-            r.pause_threshold = 1.0
-
+            r.pause_threshold = 0.5      
+            r.phrase_threshold = 0.1      
             # listening
-            audio = r.listen(source, timeout=5, phrase_time_limit=8)
+            audio = r.listen(source, timeout=10, phrase_time_limit=10)
         try:
-            text = r.recognize_google(audio, language="en-US")
+            text = r.recognize_google(audio, language="it-IT")
             print(f"You said: {text}")
             return text
         except sr.UnknownValueError:
@@ -34,17 +30,15 @@ class Voice:
     def text_to_speech(self, text):
         # init
         engine = pyttsx3.init()
-
         # get available voices
         voices = engine.getProperty('voices')
         for voice in engine.getProperty("voices"):
             if "daniel" in voice.name.lower():
                 engine.setProperty("voice", voice.id)
                 break
-        
         # parameters
-        engine.setProperty('rate', 140)    # words speed
-        engine.setProperty('volume', 0.9)  # volume (0.0 to 1.0)
+        engine.setProperty('rate', 140) # words speed
+        engine.setProperty('volume', 0.9) # volume (0.0 to 1.0)
         format_text = str(text)
         engine.say(format_text)
         engine.runAndWait()
